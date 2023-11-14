e66c4c335741d4a3a2500e7d257b20c7ecba619a
Roger Viñas <roger.vinas@n26.com>
2023-11-14 19:10:09 +0100

    Update scripts

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
+plugins {
+  id("org.springframework.boot") version "2.7.17"
+  id("io.spring.dependency-management") version "1.0.15.RELEASE"
+  java
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.assertj:assertj-core:3.22.0")
+  testImplementation("org.awaitility:awaitility:4.1.1")
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events(PASSED, SKIPPED, FAILED)
+    exceptionFormat = FULL
+    showExceptions = true
+    showCauses = true
+    showStackTraces = true
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "spring-immutable-configuration-properties"
