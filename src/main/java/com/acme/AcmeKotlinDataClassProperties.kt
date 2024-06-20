e0124760c28af3fcee2050649b71e763924e3a11
Roger Vinas <roger.vinas@fastzink.com>
2024-06-20 13:06:08 +0200

    Refactor gradle

--- src/main/java/com/acme/AcmeKotlinDataClassProperties.kt
+++ src/main/java/com/acme/AcmeKotlinDataClassProperties.kt
@@ -3,9 +3,9 @@ package com.acme
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("acme")
-data class AcmeKotlinDataClassProperties (
+data class AcmeKotlinDataClassProperties(
   val enabled: Boolean,
   val text: String,
   val list: List<String>,
-  val number: Float
+  val number: Float,
 )
